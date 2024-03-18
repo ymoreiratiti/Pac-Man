17243fa2dffeb0cec3b752e1e4e052f41d871b2c
Yuri Titi <ymoreiratiti@gmail.com>
2024-03-17 21:38:55 -0300

    feat(spritesheet): maze

--- src/resources/file-path.ts
+++ src/resources/file-path.ts
@@ -1,6 +1,8 @@
 import tmxMap001 from "../../assets/maps/map-001.tmx";
 import pngSpriteSheetActors from "../../assets/tilesets/spritesheet/actors.png";
 import tsxSpriteSheetActors from "../../assets/tilesets/spritesheet/actors.tsx";
+import pngSpriteSheetMaze from "../../assets/tilesets/spritesheet/maze.png";
+import tsxSpriteSheetMaze from "../../assets/tilesets/spritesheet/maze.tsx";
 import pngWall from "../../assets/tilesets/wall/wall.png";
 import tsxWall from "../../assets/tilesets/wall/wall.tsx";
 
@@ -9,6 +11,10 @@ export const pathMap = {
   "wall.png": pngWall,
   "wall.tsx": tsxWall,
   spritesheet: {
+    maze: {
+      png: pngSpriteSheetMaze,
+      tsx: tsxSpriteSheetMaze,
+    },
     actors: {
       png: pngSpriteSheetActors,
       tsx: tsxSpriteSheetActors,
--- src/resources/map-001.ts
+++ src/resources/map-001.ts
@@ -10,5 +10,7 @@ export const map001Resource = new TiledResource(pathMap["map-001.tmx"], {
     { path: "map-001.tmx", output: pathMap["map-001.tmx"] },
     { path: "wall.png", output: pathMap["wall.png"] },
     { path: "wall.tsx", output: pathMap["wall.tsx"] },
+    { path: "maze.png", output: pathMap.spritesheet.maze.png },
+    { path: "maze.tsx", output: pathMap.spritesheet.maze.tsx },
   ],
 });
